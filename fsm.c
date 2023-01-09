883e7a7e89bfb3d8f71e5de0e176667f56f2ca3e
Abdul Moiz <114371948+abdulmoiz5656@users.noreply.github.com>
2023-01-09 20:54:04 +0700

    Update fsm.c

--- fsm.c
+++ fsm.c
@@ -1,72 +1,91 @@
+#include <stdio.h>
 
- class CoffeeMachine {
-  User user;
-  state {
-    idle {
-      coin(int value) [value == 10] -> paidTen; }
-    paidTen {
-       coffee / {user.cupOfCoffee(new Drink());}  -> idle; }
-  }
- }
 
- 
-public boolean coffee() {
-      boolean wasEventProcessed = false;
-      State aState = state;
-      switch (aState) {
-            case paidTen:
-                  user.cupOfCoffee(new Drink());
-                  setState(State.idle);
-                  wasEventProcessed = true;
-                  break;
-            }
-      return wasEventProcessed;
+void insert_coin(int* pcustomer_coin)
+{
+    printf("| Insert Coin: ");
+    scanf("%d", pcustomer_coin);
 }
 
-public class Main {
-      public static Delivery del = new Delivery();
-      public static CoffeeMachine cm =
-            new CoffeeMachine(del);
-      public static User user = new User(cm, del);
-      public static void main ( String[] args ) {
-            new Thread(user).start(); }
+
+void move_coin(int *from, int* to)
+{
+    *to = *from;
+    *from -= *from;
 }
 
-public class Delivery {
-      public Drink coffee = null;
-      public int returnedCoins = 0;
-      public void cupOfCoffee(Drink d) {
-            coffee = d; }
+
+void move_coin_after(int *from, int* to, int after)
+{
+    *from -= after;
+    move_coin(from, to);
 }
 
-public class User implements Runnable {
-      private CoffeeMachine myCM; Delivery d;
-      public User(CoffeeMachine m, Delivery del) {
-            myCM = m; d = del; }
-      public void run(){
-            while (true) {
-                  myCM.coin(10);
-                  myCM.coffee();
-                  try {Thread.sleep(2000);}
-                        catch(InterruptedException ie){}
-                  if (d.coffee != null){
-                        d.coffee = null;
-                        System.out.println("got a coffee");
-                  } else {System.out.println("there is no drink");}
 
-            }
-      }
-}
+void buy_tea(int *pcustomer_coin, int* pmachine_coin)
+{
+    insert_coin(pcustomer_coin);
 
-class CoffeeMachine {
-  Delivery del;
-  state {
-    idle {
-      coin(int value) [value == 10] -> paidTen;
+    if (*pcustomer_coin < 5)
+    {
+        *pcustomer_coin = 0;
+        printf("| Failed to buy tea. Insufficient coin.\n");
+        return;
     }
-    paidTen {
-       coffee / {del.cupOfCoffee(new Drink());}
-         -> idle;
-     }
-  }
+
+    move_coin_after(pcustomer_coin, pmachine_coin, 5);
+    printf("| Tea purchased. Coin left: %d\n", *pcustomer_coin);
+}
+
+
+void buy_coffee(int *pcustomer_coin, int* pmachine_coin)
+{
+    insert_coin(pcustomer_coin);
+
+    if (*pcustomer_coin < 10)
+    {
+        *pcustomer_coin = 0;
+        printf("| Failed to buy coffee. Insufficient coin.\n");
+        return;
+    } 
+
+    move_coin_after(pcustomer_coin, pmachine_coin, 10);
+    printf("| Coffee purchased.\n");
+}
+
+
+void refund_excess(int* pcustomer_coin, int* pmachine_coin)
+{
+    printf("| Refunding excess %d coin\n", *pmachine_coin);
+    move_coin(pmachine_coin, pcustomer_coin);
+}
+
+
+void purchase_input(int* ppurchase_id, int* pcoin)
+{
+    printf("Input for purchase index [1 Coffee | 2 Tea]: ");
+    scanf("%d", ppurchase_id);
+}
+
+
+int main()
+{    
+    int customer_coin = 0,
+        machine_coin = 0,
+        purchase_id = 0;
+
+    purchase_input(&purchase_id, &customer_coin);
+    for(;; purchase_input(&purchase_id, &customer_coin))
+    {
+        switch (purchase_id)
+        {
+            case 1: buy_coffee(&customer_coin, &machine_coin); break;
+            case 2: buy_tea(&customer_coin, &machine_coin); break;
+        }
+
+        if (machine_coin > 0)
+            refund_excess(&customer_coin, &machine_coin);
+    }
+
+    return 0;
 }
